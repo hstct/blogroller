0c50504bc11572bca805739223f2d9ad5d451347
hstct <code@elmc.at>
2024-12-31 00:56:15 +0100

    config: add linting and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+    {
+        // Define environment settings
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...js.configs.recommended.languageOptions.globals,
+                browser: true,
+                node: true,
+            },
+        },
+        plugins: {
+            prettier: pluginPrettier,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...configPrettier.rules,
+            "prettier/prettier": "error",
+        },
+    },
+];
