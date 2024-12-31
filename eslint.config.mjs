1261481b421160ea393bb4ebe6f0429c3ac7d778
hstct <code@elmc.at>
2024-12-31 01:10:30 +0100

    chore: eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import globals from 'globals';
 import js from '@eslint/js';
 import pluginPrettier from 'eslint-plugin-prettier';
-import configPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
@@ -10,9 +10,9 @@ export default [
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: {
-        ...js.configs.recommended.languageOptions.globals,
-        browser: true,
-        node: true,
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
       },
     },
     plugins: {
@@ -20,7 +20,6 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...configPrettier.rules,
       'prettier/prettier': 'error',
     },
   },
